10911ba8b39b4002e2c96e68cd3a87bef987341f
Hauke <hank.la.boon@gmail.com>
2024-11-08 12:30:15 +0100

    äd toggle function to bookmark

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const bookmarkImg = document.querySelector(`[data-js="quiz-card--bookmark"]`);
+console.log(bookmarkImg);
+
+bookmarkImg.addEventListener("click", () => {
+  // First approach:
+  //   bookmarkImg.src === "./images/bookmark-plus.svg"
+  //     ? "./images/star.svg"
+  //     : "./images/bookmark-plus.svg";
+
+  // Second approach:
+  //   if (bookmarkImg.src === "./images/bookmark-plus.svg") {
+  //     bookmarkImg.src = "./images/star.svg";
+  //   } else {
+  //     bookmarkImg.src = "./images/bookmark-plus.svg";
+  //   }
+
+  // Third approach:
+  //     bookmarkImg.classList.toggle("active");
+  //   bookmarkImg.classList.toggle("inactive");
+
+  //   Fourth approach:
+  bookmarkImg.classList.toggle("inactive");
+  bookmarkImg.classList.toggle("active");
+});
